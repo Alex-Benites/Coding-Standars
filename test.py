a1821d2e706364fe883587a69c2f478c9d124186
Alex-Benites <136673432+Alex-Benites@users.noreply.github.com>
2025-05-29 16:34:38 -0500

    Codigo ya solucionado los problemas

--- test.py
+++ test.py
@@ -1,4 +1,13 @@
-class student:
+"""
+This module defines the Student class and demonstrates its functionality.
+It includes methods for managing grades, calculating averages, and generating reports.
+"""
+
+class Student:
+    """
+    Represents a student with an ID, name, grades, and other attributes.
+    Provides methods to manage grades and calculate averages.
+    """
     def __init__(self, student_id, name):
         self.id = student_id
         self.name = name
@@ -6,43 +15,85 @@ class student:
         self.is_passed = "NO"
         self.honor = "?"
 
-    def addGrades(self, g):
-        self.grades.append(g)
+    def add_grades(self, grade):
+        """
+        Adds a grade to the student's list of grades.
 
-    def calcaverage(self):
+        Args:
+            grade (int): The grade to add.
+        """
+        self.grades.append(grade)
+
+    def calc_average(self):
+
+        """
+        Calculates the average of the student's grades.
+
+        Returns:
+            float: The average grade, or 0 if there are no grades.
+        """
+
         if len(self.grades) == 0:
             return 0
-        t = 0
-        for x in self.grades:
-            t += x
-        avg = t / len(self.grades)
+        total = 0
+        for grade in self.grades:
+            total += grade
+        avg = total / len(self.grades)
         return avg
 
-    def checkHonor(self):
-        if self.calcaverage() > 90:
+    def check_honor(self):
+
+        """
+        Checks if the student's average grade is above 90.
+        If so, sets the honor attribute to 'yep'.
+        """
+        if self.calc_average() > 90:
             self.honor = "yep"
 
-    def deleteGrade(self, index):
+    def delete_grade(self, index):
+
+        """
+        Deletes a grade from the student's list of grades by index.
+
+        Args:
+            index (int): The index of the grade to delete.
+
+        Raises:
+            IndexError: If the index is out of range.
+        """
+
+
         if 0 <= index < len(self.grades):
             del self.grades[index]
         else:
             print("Invalid index")
 
     def report(self):
+
+        """
+        Prints a report of the student's details, including ID, name,
+        number of grades, and average grade.
+        """
+
         print("ID: " + str(self.id))
         print("Name is: " + self.name)
         print("Grades Count: " + str(len(self.grades)))
-        print("Average Grade: " + str(self.calcaverage()))
+        print("Average Grade: " + str(self.calc_average()))
 
 
-def startrun():
-    a = student("x", "")
-    a.addGrades(100)
-    a.addGrades("Fifty")  # broken
-    a.calcaverage()
-    a.checkHonor()
-    a.deleteGrade(5)  # IndexError
+def start_run():
+    """
+    Demonstrates the functionality of the Student class by creating
+    a student, adding grades, calculating averages, and printing a report.
+    """
+
+    a = Student("x", "")
+    a.add_grades(100)
+    a.add_grades(50)  # broken
+    a.calc_average()
+    a.check_honor()
+    a.delete_grade(0)  # IndexError
     a.report()
 
 
-startrun()
+start_run()
